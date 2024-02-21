082628bafd4277eff95d40d54392492d007117ff
Schuyler deVos <schuylerdevos@gmail.com>
2024-02-21 15:03:52 -0500

    add swipe capabilities

--- hooks/useKeyDown.ts
+++ hooks/useKeyDown.ts
@@ -5,14 +5,18 @@ import { updateShaderIndex } from "@/store/slice";
 export const useShaderUpdater = (): void => {
   const shaderIndex = useAppSelector((state) => state.shaderIndex);
   const dispatch = useAppDispatch();
+
   useEffect(() => {
+    let touchstartX = 0;
+    let touchendX = 0;
+
     const onKeyDown = (event) => {
-      const GoingUp = event.key === "ArrowRight";
-      const GoingDown = event.key === "ArrowLeft";
-      if (GoingUp) {
+      const goingUp = event.key === "ArrowRight";
+      const goingDown = event.key === "ArrowLeft";
+      if (goingUp) {
         event.preventDefault();
         shaderIndex < 6 ? dispatch(updateShaderIndex(shaderIndex + 1)) : null;
-      } else if (GoingDown) {
+      } else if (goingDown) {
         event.preventDefault();
         shaderIndex > 0 ? dispatch(updateShaderIndex(shaderIndex - 1)) : null;
       } else {
@@ -20,9 +24,34 @@ export const useShaderUpdater = (): void => {
       }
     };
 
+    const onFingerSwipe = (event): void => {
+      touchstartX = event.changedTouches[0].screenX;
+    };
+
+    const onTouchEnd = (event): void => {
+      touchendX = event.changedTouches[0].screenX;
+
+      checkDirection();
+
+      function checkDirection() {
+        if (touchendX < touchstartX) {
+          event.preventDefault();
+          shaderIndex < 6 ? dispatch(updateShaderIndex(shaderIndex + 1)) : null;
+        }
+        if (touchendX > touchstartX) {
+          event.preventDefault();
+          shaderIndex > 0 ? dispatch(updateShaderIndex(shaderIndex - 1)) : null;
+        }
+      }
+    };
+
     document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("touchstart", onFingerSwipe);
+    document.addEventListener("touchend", onTouchEnd);
     return () => {
       document.removeEventListener("keydown", onKeyDown);
+      document.removeEventListener("touchstart", onFingerSwipe);
+      document.removeEventListener("touchend", onTouchEnd);
     };
   }, [shaderIndex, dispatch]);
 };
