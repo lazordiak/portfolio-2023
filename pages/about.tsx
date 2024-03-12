66efde7c9102b0260c6d691ed5d5754d4d8e037d
Schuyler deVos <schuylerdevos@gmail.com>
2024-03-12 11:42:57 -0400

    quick styling changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,9 +5,12 @@ export default function About() {
   return (
     <div className="h-screen w-screen">
       <Header />
-      <div className="py-12 lg:py-24 px-6 lg:px-72 flex flex-col lg:flex-row items-center lg:justify-between text-snow text-lg">
+      <div className="py-12 lg:py-24 px-6 lg:px-72 flex flex-col lg:flex-row text-snow text-lg">
         <div className="flex-1 flex lg:pr-24 flex-col">
-          <span className="text-2xl font-medium">About Schuyler</span>
+          <span className="text-4xl font-medium">Schuyler deVos</span>
+          <span className="text-2xl mt-6">
+            Software engineer, creative technologist.
+          </span>
           <span className="mt-6">
             By day, Schuyler makes money doing software engineering things like
             building websites, hooking up backends and appeasing product
@@ -19,8 +22,8 @@ export default function About() {
             eating hot pot or taking care of plants.
           </span>
           <span className="mt-6">
-            He is a fan of all things arty, traveling and meeting new people. If
-            you&apos;re reading this, you could be one of them!
+            He is a fan of all things artsy, traveling and meeting new people.
+            If you&apos;re reading this, you could be one of them!
           </span>
         </div>
         <div
